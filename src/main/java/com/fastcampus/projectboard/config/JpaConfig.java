7336b9286537cafac9874dd44cc8111f230c584d
bumja <gngk1234@naver.com>
2023-03-02 18:05:11 +0900

    #10 - Spring Data JPA 속성과 설정
    * TODO: 패스워드 직접 노출 -> 처리 필요

---
+++ src/main/java/com/fastcampus/projectboard/config/JpaConfig.java
@@ -0,0 +1,20 @@
+package com.fastcampus.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("bumja"); // TODO: 스프링 시큐리티로 인증 기능 추가 시 수정
+
+    }
+
+}
